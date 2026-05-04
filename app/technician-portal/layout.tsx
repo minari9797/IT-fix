c74968c5919259c1336415634e2581f0ea68d9a6
Imene <i_bouchareb@estin.dz>
2026-05-04 21:49:33 +0100

    fix: remove double SidebarProvider in tech portal layout + Coffee import

--- app/technician-portal/layout.tsx
+++ app/technician-portal/layout.tsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import { usePathname } from 'next/navigation'
 import TechnicianSidebar from '@/components/layout/TechnicianSidebar'
-import { SidebarProvider } from '@/lib/context/SidebarContext'
 
 export default function TechnicianPortalLayout({
   children,
@@ -12,25 +11,19 @@ export default function TechnicianPortalLayout({
 }) {
   const pathname = usePathname()
   
-  // Use a more robust check to detect the login page
   const isLoginPage = !pathname || pathname.startsWith('/technician-portal/login')
 
   if (isLoginPage) {
-    return (
-      <SidebarProvider>
-        {children}
-      </SidebarProvider>
-    )
+    return <>{children}</>
   }
 
   return (
-    <SidebarProvider>
-      <div className="flex">
-        <TechnicianSidebar />
-        <div className="flex-1 min-h-screen">
-          {children}
-        </div>
+    <div className="flex">
+      <TechnicianSidebar />
+      <div className="flex-1 min-h-screen">
+        {children}
       </div>
-    </SidebarProvider>
+    </div>
   )
 }
+
